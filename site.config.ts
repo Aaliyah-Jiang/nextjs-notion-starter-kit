28ec5015100a8ac47ba435d6c6b2d0db7f696961
Aaliyah Jiang <123627350+Aaliyah-Jiang@users.noreply.github.com>
2023-04-28 03:59:00 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   twitter: 'Aaliyah_Jiang',
-  github: '',
+  instagram: 'aaliyah_jiang',
   linkedin: '',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
